6612e11545e4a2f9f90d86a5e92c4320d9822301
Justin Winger <justinwinger4@gmail.com>
2020-04-08 17:08:37 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,14 +7,18 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person 
+      raise PartnerError
+    else 
+     person.partner = self
+   end
   end
-
-end
 
 class PartnerError < StandardError 
   
 end
+end
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
